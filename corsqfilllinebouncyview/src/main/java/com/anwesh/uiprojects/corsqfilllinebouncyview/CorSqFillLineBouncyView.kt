1778f6c7eb5b20bd2087250052a02268623c5526
Anwesh43 <anweshthecool0@gmail.com>
2020-01-05 13:10:04 +0530

    adding CorSqFillLineBouncyView class

--- corsqfilllinebouncyview/src/main/java/com/anwesh/uiprojects/corsqfilllinebouncyview/CorSqFillLineBouncyView.kt
+++ corsqfilllinebouncyview/src/main/java/com/anwesh/uiprojects/corsqfilllinebouncyview/CorSqFillLineBouncyView.kt
@@ -62,3 +62,21 @@ fun Canvas.drawCSFLBNode(i : Int, scale : Float, paint : Paint) {
     drawCorFillSquare(size, scale, paint)
     restore()
 }
+
+class CorSqFillLineBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
