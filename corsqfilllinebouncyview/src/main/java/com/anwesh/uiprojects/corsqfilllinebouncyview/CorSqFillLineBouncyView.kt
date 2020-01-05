e0b9927da9448185dc4922edd982bbe88b9bdb36
Anwesh43 <anweshthecool0@gmail.com>
2020-01-05 12:49:17 +0530

    adding constants and libraries

--- corsqfilllinebouncyview/src/main/java/com/anwesh/uiprojects/corsqfilllinebouncyview/CorSqFillLineBouncyView.kt
+++ corsqfilllinebouncyview/src/main/java/com/anwesh/uiprojects/corsqfilllinebouncyview/CorSqFillLineBouncyView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.corsqfilllinebouncyview
 /**
  * Created by anweshmishra on 05/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
