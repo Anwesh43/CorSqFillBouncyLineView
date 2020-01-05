6853f760af593d19392fc65a0968c09b686a948e
Anwesh43 <anweshthecool0@gmail.com>
2020-01-05 13:28:20 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcorsqfilllinebouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcorsqfilllinebouncyview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedcorsqfilllinebouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.corsqfilllinebouncyview.CorSqFillLineBouncyView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CorSqFillLineBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
